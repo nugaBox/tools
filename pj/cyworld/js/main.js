6daf4ff2bcaf62bd8e61d01759f635fae06b28b4
nugaBox <ngjang@kakao.com>
2021-04-09 00:57:26 +0900

    cyworld remove

--- pj/cyworld/js/main.js
+++
@@ -1,5 +0,0 @@
-function handleClickMenu(e) {
-    let menuList = document.getElementsByClassName("menu-item");
-    [...menuList].forEach(x => x.className = x.className.replace(/\bmenu-selected\b/g, ""));
-    e.classList.add("menu-selected");
-  }
